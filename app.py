25a121f29b81f0d9ff64119ef4a7a94fc24ef690
LucaBarboza <luca.barboza@gmail.com>
2025-12-09 00:06:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(
 
 # Configuração da navegação entre páginas
 paginas = {
-    st.Page('paginas/home.py', title='Home', default=True)
+    st.Page('paginas/home.py', title='Home', default=True),
     'Análise de dados': [
         st.Page('paginas/dashboard.py', title='Dashboard Interativo das Análises Climáticas', default=False),
         st.Page('paginas/mapa.py', title='Mapa Temporal Climático', default=False)
