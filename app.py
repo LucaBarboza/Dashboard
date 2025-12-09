18c9938b48d0b53a0fe27b1be2ad1429e4ad3766
LucaBarboza <luca.barboza@gmail.com>
2025-12-08 23:57:34 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Análise Climática",
+    page_icon="⛅",
+    layout="wide"
+)
+
+# Configuração da navegação entre páginas
+paginas = {
+    'Análise de dados': [
+        st.Page('paginas/dashboard.py', title='Dashboard Interativo das Análises Climáticas', default=True),
+        st.Page('paginas/mapa.py', title='Mapa Temporal Climático', default=True)
+    ]
+}
+
+# Execução da navegação
+pag = st.navigation(paginas)
+pag.run()
