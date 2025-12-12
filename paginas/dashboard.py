1084a32e5d8b8fd645c03646eb4cc13032d2a754
LucaBarboza <luca.barboza@gmail.com>
2025-12-11 23:13:40 -0300

    .

--- paginas/dashboard.py
+++ paginas/dashboard.py
@@ -123,7 +123,7 @@ fig_box = px.box(
     y=var_coluna, 
     color=cor_grafico, 
     title=f"Boxplot de {var_label}{sulfixo_titulo}",
-    template="simple_white"
+    template="plotly_white"
 )
 
 if not estados_filtro:
@@ -141,7 +141,6 @@ fig_box.update_layout(
         tickfont=dict(color='black'),   
         title_font=dict(color='black')  
     ),
-    template='plotly_white',
     showlegend=False
 )
 
