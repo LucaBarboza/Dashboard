a69c3a32b0c39faf99942ab9c0321ac316d4a025
lutz978 <vitorrfreitass2006@gmail.com>
2025-12-18 23:09:49 -0300

    app.py

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ paginas = {
         st.Page('paginas/dashboard.py', title='Dashboard Interativo', icon = "📎", default=False),
         st.Page('paginas/mapa.py', title='Mapa Temporal Climático', icon = "🗺️",default=False),
         st.Page('paginas/estatistica.py', title='Matrizes de Correlação', icon="📊"),
-        st.Page('paginas/modelagem.py', title='Modelagem e Previsão', icon="🤖")
+        st.Page('paginas/modelagem.py', title='Modelagem e Previsão', icon="🤖"),
+        st.Page('paginas/testes.py', title='Teste de hipótese', icon="☑️")
     ]
 }
 
